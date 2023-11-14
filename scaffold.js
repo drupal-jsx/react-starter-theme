b32f5bc5de84784ee1d7255593ce3053a05283f1
effulgentsia <alex.bronstein@acquia.com>
2023-11-14 13:21:47 -0800

    Delete scaffold.js

--- scaffold.js
+++
@@ -1,8 +0,0 @@
-import packageInfo from "./package.json";
-
-const drupalThemeName = packageInfo.name.replaceAll('-', '_');
-
-const f = Bun.file('scaffold/drupal/info.yml');
-const scaffoldInfo = await f.text();
-const info = scaffoldInfo.replaceAll('NAME', drupalThemeName);
-await Bun.write(`${drupalThemeName}.info.yml`, info);
