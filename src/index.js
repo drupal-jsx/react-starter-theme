4dda8d1e8c4cd3a282043e9786642c22386638c0
effulgentsia <alex.bronstein@acquia.com>
2023-11-14 10:29:43 -0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { createElement as h, Fragment } from 'react';
+import { createRoot } from 'react-dom/client';
+import hyperscriptify from 'hyperscriptify';
+import propsify from 'hyperscriptify/propsify/standard';
+
+import components from './components';
+components['drupal-html-fragment'] = Fragment;
+
+document.querySelectorAll('template[hyperscriptify]').forEach(function(templateElement) {
+  const App = hyperscriptify(templateElement.content, h, Fragment, components, { propsify });
+  const container = document.createElement('div');
+  templateElement.after(container);
+  const root = createRoot(container);
+  root.render(App);
+});
