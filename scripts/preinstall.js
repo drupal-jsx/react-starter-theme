7945a34c436954c51e41558333a536860323dba4
effulgentsia <alex.bronstein@acquia.com>
2023-11-14 13:24:23 -0800

    Create preinstall.js

---
+++ scripts/preinstall.js
@@ -0,0 +1,8 @@
+import packageInfo from "../package.json";
+
+const drupalThemeName = packageInfo.name.replaceAll('-', '_');
+
+const f = Bun.file('scaffold/drupal/info.yml');
+const scaffoldInfo = await f.text();
+const info = scaffoldInfo.replaceAll('NAME', drupalThemeName);
+await Bun.write(`${drupalThemeName}.info.yml`, info);
