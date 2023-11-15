d91521ad29dc87557f2488696fb051ab1f4924c0
effulgentsia <alex.bronstein@acquia.com>
2023-11-14 18:33:43 -0800

    Update components.js

--- src/components.js
+++ src/components.js
@@ -1,7 +1,10 @@
 import DrupalPage from './components/DrupalPage';
+import { Link, Button } from '@radix-ui/themes';
 
 const components = {
   'drupal-page': DrupalPage,
+  'a': Link,
+  'button': Button,
 };
 
 export default components;
