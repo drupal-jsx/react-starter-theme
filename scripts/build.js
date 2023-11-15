decdf81a709f7eeef05f1a9d46c083af553e7c10
effulgentsia <alex.bronstein@acquia.com>
2023-11-14 19:39:02 -0800

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,5 @@
 import * as esbuild from 'esbuild';
+import components from '../src/components';
 
 const config = {
   logLevel: 'info',
@@ -6,7 +7,14 @@ const config = {
   outfile: 'dist/app.js',
   bundle: true,
   minify: true,
-  jsx: 'automatic',  
+  jsx: 'automatic',
 }
 
 await esbuild.build(config);
+
+for (const tagName in components) {
+  if (tagName.startsWith('drupal-') && components[tagName].drupalPropTypes) {
+    const templateName = tagName.substring(7);
+    await Bun.write(`dist/prop-types/${templateName}.template-info.json`, JSON.stringify({props: components[tagName].drupalPropTypes}));
+  }
+}
