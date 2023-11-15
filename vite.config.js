9ef3061fd5e9a3585f8909f61951b67b6cce9485
effulgentsia <alex.bronstein@acquia.com>
2023-11-14 21:01:42 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
+import react from "@vitejs/plugin-react-swc";
+
 export default {
+  plugins: [react()],
+  
   // @see https://vitejs.dev/guide/backend-integration.html.
   build: {
     manifest: true,
