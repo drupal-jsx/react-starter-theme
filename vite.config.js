924d73cd0f9deeb06de2a4a912306828cc8a1dcb
effulgentsia <alex.bronstein@acquia.com>
2023-12-02 06:48:41 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
+import drupal from "@drupal-jsx/vite-plugin-drupal";
 
 export default {
-  plugins: [react()],
-  
-  // @see https://vitejs.dev/guide/backend-integration.html.
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: 'src/index.js',
-    },
-  },
+  root: 'src',
+  plugins: [react(), drupal()],
 }
