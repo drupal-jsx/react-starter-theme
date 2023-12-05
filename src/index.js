16db02155a05ec3da52fb011dff47834ed3adbff
effulgentsia <alex.bronstein@acquia.com>
2023-12-05 10:15:58 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-import { createElement as h, Fragment } from 'react';
-import { createRoot } from 'react-dom/client';
-import hyperscriptify from 'hyperscriptify';
-import propsify from 'hyperscriptify/propsify/standard';
+import { main } from '@drupal-jsx/drupal-utils-react';
+import components from './components';
 import './index.css';
 
-import components from './components';
-components['drupal-html-fragment'] = Fragment;
-
-document.querySelectorAll('template[hyperscriptify]').forEach(function(templateElement) {
-  const App = hyperscriptify(templateElement.content, h, Fragment, components, { propsify });
-  const container = document.createElement('div');
-  templateElement.after(container);
-  const root = createRoot(container);
-  root.render(App);
-});
+main({ components });
