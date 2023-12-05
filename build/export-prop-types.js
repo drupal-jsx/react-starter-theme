94ef19710c6c876e70803eb0c394fd4da1d03e82
effulgentsia <alex.bronstein@acquia.com>
2023-12-05 13:16:07 -0800

    Update export-prop-types.js

--- build/export-prop-types.js
+++ build/export-prop-types.js
@@ -1,3 +1,3 @@
-import { exportPropTypes } from '@drupal-jsx/drupal-utils/build';
+import { exportPropTypes } from '@drupal-jsx/drupal-utils-dev';
 
 await exportPropTypes(process.cwd() + '/src/components.js', 'dist/prop-types');
