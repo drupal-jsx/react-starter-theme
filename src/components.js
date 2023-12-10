65bc20b026e4203c23e8eea1443fff972a026001
effulgentsia <alex.bronstein@acquia.com>
2023-12-09 21:36:32 -0800

    Delete src/components.js

--- src/components.js
+++
@@ -1,6 +0,0 @@
-import { componentsFromModules } from '@drupal-jsx/drupal-utils';
-const modules = import.meta.glob('./components/Drupal*.jsx', { eager: true })
-
-const components = componentsFromModules(modules);
-
-export default components;
