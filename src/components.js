19ac434a85b6cd012da4529e55d444692513f994
effulgentsia <alex.bronstein@acquia.com>
2023-12-09 21:30:17 -0800

    Update components.js

--- src/components.js
+++ src/components.js
@@ -1,7 +1,6 @@
-import DrupalPage from './components/DrupalPage';
+import { componentsFromModules } from '@drupal-jsx/drupal-utils';
+const modules = import.meta.glob('./components/Drupal*.jsx', { eager: true })
 
-const components = {
-  'drupal-page': DrupalPage,
-};
+const components = componentsFromModules(modules);
 
 export default components;
