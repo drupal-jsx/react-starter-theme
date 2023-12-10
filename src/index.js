5ba4df9a258f37256835c9812083d96e6bdc8028
effulgentsia <alex.bronstein@acquia.com>
2023-12-09 21:35:51 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { main } from '@drupal-jsx/drupal-utils-react';
-import components from './components';
 import './index.css';
+
+import { componentsFromModules } from '@drupal-jsx/drupal-utils';
+const modules = import.meta.glob('./components/Drupal*.jsx', { eager: true })
+const components = componentsFromModules(modules);
 
 main({ components });
