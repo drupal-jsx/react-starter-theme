ddf35410806583c896f8f0070f215fb8677a9e96
effulgentsia <alex.bronstein@acquia.com>
2023-12-10 20:10:50 -0800

    Create DrupalLayout--TwocolSection.jsx

---
+++ src/components/DrupalLayout--TwocolSection.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import { props } from '@drupal-jsx/drupal-utils';
+import clsx from 'clsx';
+
+export default function DrupalLayout__TwocolSection({ attributes, content, regionAttributes }) {
+  if (!content) {
+    return <></>;
+  }
+
+  return(
+    <div { ...props(attributes) } className="flex flex-row justify-between" >
+      { content.first &&
+        <div { ...props(regionAttributes?.first) } className={ clsx(regionAttributes?.first?.class, 'basis-1/2') }>
+          { content.first }
+        </div>
+      }
+      { content.second &&
+        <div { ...props(regionAttributes?.second) } className={ clsx(regionAttributes?.second?.class, 'basis-1/2') }>
+          { content.second }
+        </div>
+      }
+    </div>
+  );
+}
+
+DrupalLayout__TwocolSection.propTypes = {
+  attributes: PropTypes.object,
+  content: PropTypes.shape({
+    first: PropTypes.element,
+    second: PropTypes.element,
+  }),
+  regionAttributes: PropTypes.shape({
+    first: PropTypes.object,
+    second: PropTypes.object,
+  }),
+}
