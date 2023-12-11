f9ff7ee7aec68576f4ff8c2095e80cdd3c427586
effulgentsia <alex.bronstein@acquia.com>
2023-12-11 06:31:49 -0800

    Update DrupalLayout--ThreecolSection.jsx

--- src/components/DrupalLayout--ThreecolSection.jsx
+++ src/components/DrupalLayout--ThreecolSection.jsx
@@ -10,17 +10,17 @@ export default function DrupalLayout__ThreecolSection({ attributes, content, reg
   return(
     <div { ...props(attributes) } className="flex flex-row justify-between" >
       { content.first &&
-        <div { ...props(regionAttributes?.first) } className={ clsx(regionAttributes?.first?.class, 'basis-1/3') }>
+        <div { ...props(regionAttributes?.first) } className={ clsx(regionAttributes?.first?.class, 'w-1/3') }>
           { content.first }
         </div>
       }
       { content.second &&
-        <div { ...props(regionAttributes?.second) } className={ clsx(regionAttributes?.second?.class, 'basis-1/3') }>
+        <div { ...props(regionAttributes?.second) } className={ clsx(regionAttributes?.second?.class, 'w-1/3') }>
           { content.second }
         </div>
       }
       { content.third &&
-        <div { ...props(regionAttributes?.third) } className={ clsx(regionAttributes?.third?.class, 'basis-1/3') }>
+        <div { ...props(regionAttributes?.third) } className={ clsx(regionAttributes?.third?.class, 'w-1/3') }>
           { content.third }
         </div>
       }
