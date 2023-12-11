84b398dc7c43a914f92e44157cfa9251363ea601
effulgentsia <alex.bronstein@acquia.com>
2023-12-11 07:05:19 -0800

    Update export-prop-types.js

--- build/export-prop-types.js
+++ build/export-prop-types.js
@@ -5,3 +5,6 @@ import { Glob } from 'bun';
 await mkdir('dist/prop-types', { recursive: true });
 const glob = new Glob('src/components/Drupal*.jsx');
 await exportPropTypes(glob.scan('.'), 'dist/prop-types');
+
+const drushPath = 'drush';
+['theme-registry', 'render'].forEach((type) => Bun.spawn([drushPath, 'cache:clear', type]));
