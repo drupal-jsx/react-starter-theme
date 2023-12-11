c1e2c208b85b0db5620f9e53a54af025556bdfc7
effulgentsia <alex.bronstein@acquia.com>
2023-12-11 12:51:04 -0800

    Exporting import.meta.hot to global Drupal object for use by non-module js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ const modules = import.meta.glob('./components/Drupal*.jsx', { eager: true })
 const components = componentsFromModules(modules);
 
 main({ components });
+
+if (import.meta.hot) {
+  Drupal.jsxHot = import.meta.hot;
+}
