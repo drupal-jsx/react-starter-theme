8a3795b88dd6fd424fda885b7b499a1b3e6ec4fa
effulgentsia <alex.bronstein@acquia.com>
2023-12-17 14:49:31 -0800

    Renamed props() function

--- src/components/DrupalLayout--ThreecolSection.jsx
+++ src/components/DrupalLayout--ThreecolSection.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { props } from '@drupal-jsx/drupal-utils';
+import { attributesToProps as p } from '@drupal-jsx/drupal-utils';
 import clsx from 'clsx';
 
 export default function DrupalLayout__ThreecolSection({ attributes, content, regionAttributes }) {
@@ -8,19 +8,19 @@ export default function DrupalLayout__ThreecolSection({ attributes, content, reg
   }
 
   return(
-    <div { ...props(attributes) } className="flex flex-row flex-wrap justify-between three-col__container" >
+    <div { ...p(attributes) } className="flex flex-row flex-wrap justify-between three-col__container" >
       { content.first &&
-        <div { ...props(regionAttributes?.first) } className={ clsx(regionAttributes?.first?.class, 'w-1/3 three-col__col') }>
+        <div { ...p(regionAttributes?.first) } className={ clsx(regionAttributes?.first?.class, 'w-1/3 three-col__col') }>
           { content.first }
         </div>
       }
       { content.second &&
-        <div { ...props(regionAttributes?.second) } className={ clsx(regionAttributes?.second?.class, 'w-1/3 three-col__col') }>
+        <div { ...p(regionAttributes?.second) } className={ clsx(regionAttributes?.second?.class, 'w-1/3 three-col__col') }>
           { content.second }
         </div>
       }
       { content.third &&
-        <div { ...props(regionAttributes?.third) } className={ clsx(regionAttributes?.third?.class, 'w-1/3 three-col__col') }>
+        <div { ...p(regionAttributes?.third) } className={ clsx(regionAttributes?.third?.class, 'w-1/3 three-col__col') }>
           { content.third }
         </div>
       }
