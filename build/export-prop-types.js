99deeb665564ce046a671e77b60751598941ec84
effulgentsia <alex.bronstein@acquia.com>
2023-12-17 15:15:22 -0800

    Added cleaning of prop-types directory and faster cache clearing

--- build/export-prop-types.js
+++ build/export-prop-types.js
@@ -1,10 +1,16 @@
-import { exportPropTypes } from '@drupal-jsx/drupal-utils-dev';
-import { mkdir } from 'node:fs/promises';
+import { exportPropTypes, invalidateThemeRegistry } from '@drupal-jsx/drupal-utils-dev';
+import { mkdir, rm } from 'node:fs/promises';
+import path from 'node:path';
 import { Glob } from 'bun';
 
-await mkdir('dist/prop-types', { recursive: true });
+await mkdir('dist', { recursive: true });
+await rm('dist/prop-types', {recursive: true, force: true});
+await mkdir('dist/prop-types');
 const glob = new Glob('src/components/Drupal*.jsx');
 await exportPropTypes(glob.scan('.'), 'dist/prop-types');
 
-const drushPath = 'drush';
-['theme-registry', 'render'].forEach((type) => Bun.spawn([drushPath, 'cache:clear', type]));
+const glob2 = new Glob('*.info.yml');
+for await (const file of glob2.scan('.')) {
+  const themeName = path.basename(file, '.info.yml');
+  invalidateThemeRegistry(themeName, {sqlitePath: '../../../sites/default/files/.sqlite'});
+}
